ba1625b103fccbffefed63ee3254570c4b3165db
VesleRuben <itzRuben96@gmail.com>
2018-02-12 14:18:02 +0100

    Lagt til mer testkode i Main.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Book> list = new ArrayList<>();
-        list.add(new Book("Sex på stallen", "råger", "porno", "Frank", "5520", "1", 2, 1));
-        list.add(new Book("Hest er best", "finn", "porno/Ikke spørr", "Frank", "5520", "1", 2, 2));
+        list.add(new Book("Sex på stallen", "råger", "porno", "Frank", "01.05.1985", "1", 2, 1));
+        list.add(new Book("Hest er best", "finn", "porno/Ikke spørr", "Frank", "19 Pil og bue", "1", 2, 2));
         list.add(new Book("Playboy", "råger", "porno", "Frank", "5520", "1", 2, 3));
-        list.add(new Book("Hvordan få orgasme", "inn", "porno", "Frank", "5520", "1", 2, 3));
-        list.add(new Book("Blowjob på kjøpesenter", "finn", "porno", "Frank", "5520", "1", 2, 5));
-        list.add(new Book("single søte jenter", "finn", "porno", "Frank", "5520", "1", 2, 7));
-        list.add(new Book("Playboy", "finn", "porno", "Frank", "5520", "1", 2, 2));
-        list.add(new Book("Playboy", "finn", "porno", "Frank", "5520", "1", 2, 2));
-        list.add(new Book("Playboy", "finn", "porno", "Frank", "5520", "2", 2, 7));
+        list.add(new Book("Hvordan få orgasme", "inn", "porno", "Frank", "11.12.2003", "1", 2, 3));
+        list.add(new Book("Blowjob på kjøpesenter", "finn", "porno", "Frank", "01.05.1985", "1", 2, 5));
+        list.add(new Book("single søte jenter", "finn", "porno", "Frank", "01.05.1985", "1", 2, 7));
+        list.add(new Book("Playboy", "finn", "porno", "Frank", "01.05.1985", "1", 2, 2));
+        list.add(new Book("Playboy", "finn", "porno", "Frank", "01.05.1985", "1", 2, 2));
+        list.add(new Book("Playboy", "finn", "porno", "Frank", "01.05.1985", "2", 2, 7));
 
         LiarRegistry reg = new LiarRegistry(list);
 
